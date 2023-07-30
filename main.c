0638043ef964cd2e936843ebb9a6e8a2e0b6e03b
E3TU <yes.0ztan@slmail.me>
2023-07-30 21:51:38 +0300

    Loop until user stops the program

--- main.c
+++ main.c
@@ -2,34 +2,41 @@
 #include <stdlib.h>
 
 int main() {
-  int num1, num2;
+  int num1, num2, stop;
   char operator;
+  stop = 0;
+  while (stop == 0) {
+    printf("Enter the operator(+, -, *, /, q to quit): ");
+    scanf("%c", &operator);
 
-  printf("Enter the operator(+,-,*,/): ");
-  scanf("%c", &operator);
+    if (operator == 'q') {
+        stop = 1;
+        break;
+    }
 
-  printf("Enter first number: ");
-  scanf("%d", &num1);
+    printf("Enter first number: ");
+    scanf("%d", &num1);
 
-  printf("Enter second number: ");
-  scanf("%d", &num2);
+    printf("Enter second number: ");
+    scanf("%d", &num2);
 
-  switch (operator) {
-  case '+':
-    printf("Result: %d\n", num1 + num2);
-    break;
-  case '-':
-    printf("Result: %d\n", num1 - num2);
-    break;
-  case '*':
-    printf("Result: %d\n", num1 * num2);
-    break;
-  case '/':
-    printf("Result: %d\n", num1 / num2);
-    break;
-  default:
-    printf("Error, you entered wrong operator");
+    switch (operator) {
+    case '+':
+      printf("Result: %d\n", num1 + num2);
+      break;
+    case '-':
+      printf("Result: %d\n", num1 - num2);
+      break;
+    case '*':
+      printf("Result: %d\n", num1 * num2);
+      break;
+    case '/':
+      printf("Result: %d\n", num1 / num2);
+      break;
+    default:
+      printf("Error, you entered wrong operator");
+    }
+
+    return 0;
   }
-
-  return 0;
 }
